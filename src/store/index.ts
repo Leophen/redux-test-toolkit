87d89a3150ed66a8cb6992977cf3779a13154127
Dorkiliu <dorkiliu@tencent.com>
2021-12-17 11:45:51 +0800

    feat: 使用 configureStore

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,11 @@
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import reducers from './reducers/index'
 
-const store = createStore(reducers)
+// const store = createStore(reducers)
+const store = configureStore({
+  reducer: reducers
+})
 export default store
 
 export type RootState = ReturnType<typeof store.getState>
